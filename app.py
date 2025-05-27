a8012a8214b7031c39a2d0453ed3a90300890a83
Akash <alampallyakash@gmail.com>
2025-05-27 18:44:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+
+ 
+IMG_SIZE = 224  
+
+ 
+model = load_model('plant_disease_prediction_model.h5')   
+
+ 
+class_names = {
+    0: 'Apple___Apple_scab',
+    1: 'Apple___Black_rot',
+    2: 'Apple___Cedar_apple_rust',
+    3: 'Apple___healthy',
+    4: 'Blueberry___healthy',
+    5: 'Cherry_(including_sour)___Powdery_mildew',
+    6: 'Cherry_(including_sour)___healthy',
+    7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+    8: 'Corn_(maize)___Common_rust_',
+    9: 'Corn_(maize)___Northern_Leaf_Blight',
+    10: 'Corn_(maize)___healthy',
+    11: 'Grape___Black_rot',
+    12: 'Grape___Esca_(Black_Measles)',
+    13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+    14: 'Grape___healthy',
+    15: 'Orange___Haunglongbing_(Citrus_greening)',
+    16: 'Peach___Bacterial_spot',
+    17: 'Peach___healthy',
+    18: 'Pepper,_bell___Bacterial_spot',
+    19: 'Pepper,_bell___healthy',
+    20: 'Potato___Early_blight',
+    21: 'Potato___Late_blight',
+    22: 'Potato___healthy',
+    23: 'Raspberry___healthy',
+    24: 'Soybean___healthy',
+    25: 'Squash___Powdery_mildew',
+    26: 'Strawberry___Leaf_scorch',
+    27: 'Strawberry___healthy',
+    28: 'Tomato___Bacterial_spot',
+    29: 'Tomato___Early_blight',
+    30: 'Tomato___Late_blight',
+    31: 'Tomato___Leaf_Mold',
+    32: 'Tomato___Septoria_leaf_spot',
+    33: 'Tomato___Spider_mites Two-spotted_spider_mite',
+    34: 'Tomato___Target_Spot',
+    35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+    36: 'Tomato___Tomato_mosaic_virus',
+    37: 'Tomato___healthy'
+}
+
+ 
+st.title('🌿 Plant Disease Detection')
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
+    
+ 
+    img = img.convert('RGB')
+    
+    
+    img = img.resize((IMG_SIZE, IMG_SIZE))   
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0  
+    img_array = np.expand_dims(img_array, axis=0)  
+    
+    # Predict
+    prediction = model.predict(img_array)
+    predicted_class_idx = np.argmax(prediction)
+    predicted_class = class_names[predicted_class_idx]
+    
+    st.success(f"🌟 **Prediction:** {predicted_class}")
